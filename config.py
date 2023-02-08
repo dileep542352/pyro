1d2f9b879b368c85cd4452ac91c65c196a84fa78
dileep542352 <118031927+dileep542352@users.noreply.github.com>
2023-02-08 09:30:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5545728732:AAGi3z4YazR_CxoqpbsjSdz3Q7yZoiW5vug")
-    API_ID = int(os.environ.get("API_ID", 7856100))
-    API_HASH = os.environ.get("API_HASH","dba409d7e8d44ab5bb689378deea1792")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID",))
+    API_HASH = os.environ.get("API_HASH","")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
 
